f8520171a9c26b67bd2bc310cda54af642a7ca1a
Leigham <itsleigham@gmail.com>
2015-02-05 04:01:28 +0000

    Create g3.hpp

---
+++ tradeitems/g3.hpp
@@ -0,0 +1,217 @@
+class Category_9005 {
+		class G36C {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+		class G36C_camo {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+		class G36_C_SD_camo {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+		class G36_C_SD_eotech {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class G36K {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class G36K_camo {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class G36A {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class G36a_Camo {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class MG36 {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class vil_G3a3 {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3a2 {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3a4 {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3a4b {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3an {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3anb {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3ZF {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3zfb {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3SG1 {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3sg1b {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3TGS {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_G3TGSb {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_AG3 {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_AG3EOT {
+		type = "trade_any_vehicle";
+		buy[] ={10000,"Coins"};
+		sell[] ={6000,"Coins"};
+	};
+	class vil_Mg3 {
+		type = "trade_any_vehicle";
+		buy[] ={12500,"Coins"};
+		sell[] ={7000,"Coins"};
+	};
+	class vil_G36KA4 {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36E {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36a2 {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_AG36 {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_AG36A2 {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_AG36KV {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_AG36KA4 {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36VA4 {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36VA4Eot {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36KVZ {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36KSK {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36KSKdes {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36KSKdesES {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36CC {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36KSKES {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36KES {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class vil_G36KV3 {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class MG36_Large {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};
+	class MG36_camo_Large {
+		type = "trade_any_vehicle";
+		buy[] ={7500,"Coins"};
+		sell[] ={4500,"Coins"};
+	};	
+};
