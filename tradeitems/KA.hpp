d6498f41d1d325e86bb6ec6d982a5f94b484e4c9
Leigham <itsleigham@gmail.com>
2015-02-05 03:38:45 +0000

    Create KA.hpp

---
+++ tradeitems/KA.hpp
@@ -0,0 +1,22 @@
+class Category_9002 {
+	class Ka52 {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class Ka52Black {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class Ka60_GL_PMC {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class Ka60_PMC {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};	
+};
