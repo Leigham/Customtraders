92e1bd460790437634e95372b0a7773fcb29e808
Leigham <itsleigham@gmail.com>
2015-02-05 03:39:15 +0000

    Create Mi24.hpp

---
+++ tradeitems/Mi24.hpp
@@ -0,0 +1,22 @@
+class Category_9000 {
+		class Mi24_V {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class Mi24_P {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class Mi24_D {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class Mi24_D_TK_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+};
