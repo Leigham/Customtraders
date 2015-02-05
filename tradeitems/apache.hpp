9e1a90000929efa6007cde4e30a111f930721c51
Leigham <itsleigham@gmail.com>
2015-02-05 03:38:25 +0000

    Create apache.hpp

---
+++ tradeitems/apache.hpp
@@ -0,0 +1,22 @@
+class Category_9001 {
+		class AH1Z {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class AH64D {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class AH64D_EP1 {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+	class BAF_Apache_AH1_D {
+		type = "trade_any_vehicle";
+		buy[] ={1200000,"Coins"};
+		sell[] ={600000,"Coins"};
+	};
+};
